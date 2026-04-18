01183e40376d12ae265211532ea186d0fbcd30ee
Venkatesan A <venkatvenkat9068@gmail.com>
2026-04-18 12:46:43 +0530

    Delete views directory

--- views/Home.jsx
+++
@@ -1,9 +0,0 @@
-
-
-import React from "react";
-
-const Home = () => {
-  return <h2>Welcome to Home Page</h2>;
-};
-
-export default Home;
--- views/Users.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Users = () => {
-  return <h2>Users Page</h2>;
-};
-
-export default Users;
